fb45f7c308ec1efc7fa2f187eb387cac5b1d55b1
Brena <brenamlaurindo@outlook.com>
2023-02-10 10:41:45 -0300

    Removendo warnings

--- pacetech/src/main/java/org/pacetech/Conversor.java
+++ pacetech/src/main/java/org/pacetech/Conversor.java
@@ -13,7 +13,7 @@ public class Conversor {
             System.out.println(".........................................................................");
             System.out.println("Conversor de Km para metro ou litro para m³");
             System.out.println(".........................................................................");
-            System.out.println("");
+            System.out.println();
             System.out.println("1 - Converter quilomêtros em metros;");
             System.out.println("2 - Converter litros em m³;");
             System.out.println("3 - Sair.");
@@ -24,13 +24,13 @@ public class Conversor {
             if (atributos.getOpcao() == 1) {
                 System.out.print("Digite o valor em km para converter em m: ");
                 atributos.setKm(scanner.nextLong());
-                System.out.println("");
+                System.out.println();
                 atributos.setMetro(atributos.getKm() * 1000);
                 System.out.println(atributos.getKm() + "km equivale a " + atributos.getMetro() + "m.");
             } else if (atributos.getOpcao() == 2) {
                 System.out.print("Digite o valor do m³ para converter em litro: ");
                 atributos.setM3(scanner.nextLong());
-                System.out.println("");
+                System.out.println();
                 atributos.setLitro(atributos.getM3() * 1000);
                 System.out.println(atributos.getM3() + "m³ equivale a " + atributos.getLitro() + "L.");
             } else if (atributos.getOpcao() != 3) {
